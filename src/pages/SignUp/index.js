8592a106c0ed2bcee39b37c850354362f577c760
Hiago Souza <hiagofss98@gmail.com>
2019-07-30 18:43:49 -0300

    Utility Unform in the Project

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,23 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
 import logo from '~/assets/logo.svg';
 
 // import { Container } from './styles';
 
 export default function SignUp() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <img src={logo} alt="GoBarber" />
 
-      <form>
-        <input type="text" placeholder="Nome completo" />
-        <input type="email" placeholder="Seu e-mail" />
-        <input type="password" placeholder="Sua senha" />
+      <Form onSubmit={handleSubmit}>
+        <Input type="text" name="name" placeholder="Nome completo" />
+        <Input type="email" name="email" placeholder="Seu e-mail" />
+        <Input type="password" name="password" placeholder="Sua senha" />
 
         <button type="submit">Criar conta</button>
         <Link to="/">Já tenho login</Link>
-      </form>
+      </Form>
     </>
   );
 }
